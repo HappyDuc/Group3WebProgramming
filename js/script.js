f70911867863017cf6320cfd5acbd07ff02c5540
Jake Sinclair <sinclairs1234@gmail.com>
2024-09-23 15:56:00 +0100

    Idek

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
 $(function () {
   $(".nav-placeholder").load("/htmlTemplates/navbar.html");
 
-  // Add slideDown animation to Bootstrap dropdown when expanding.
-  $('.dropdown').on('show.bs.dropdown', function() {
-    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-  });
-
-  // Add slideUp animation to Bootstrap dropdown when collapsing.
-  $('.dropdown').on('hide.bs.dropdown', function() {
-    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-  });
-
-  $(".dropdown-button").hover(
+  $("#navbarDropdownMenuLink").hover(
     function () {
+      console.log("Hover");
       $(this).addClass("dropdown-button");
     },
     function () {
       $(this).removeClass("dropdown-button");
     }
   );
+
+  // Add slideDown animation to Bootstrap dropdown when expanding.
+  $(".dropdown").on("show.bs.dropdown", function () {
+    $(this).find(".dropdown-menu").first().stop(true, true).slideDown();
+  });
+
+  // Add slideUp animation to Bootstrap dropdown when collapsing.
+  $(".dropdown").on("hide.bs.dropdown", function () {
+    $(this).find(".dropdown-menu").first().stop(true, true).slideUp();
+  });
 
   //$(".dropdown-button").click(function (e) {
   //  e.preventDefault();
