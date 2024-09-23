ca74884aaea435dfd8e8b25e5b9f5c933384e548
Jake Sinclair <sinclairs1234@gmail.com>
2024-09-23 13:54:04 +0100

    Jake's Version

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 $(function () {
+  $(".nav-placeholder").load("/htmlTemplates/navbar.html");
+
   $(".dropdown").on("show.bs.dropdown", function () {
     $(this).find(".dropdown-menu").first().stop(true, true).slideDown();
   });
 
-  // Add slideUp animation to Bootstrap dropdown when collapsing.
   $(".dropdown").on("hide.bs.dropdown", function () {
     $(this).find(".dropdown-menu").first().stop(true, true).slideUp();
+  });
+
+  $(".dropdown-button").hover(
+    function () {
+      $(this).addClass("dropdown-toggle");
+    },
+    function () {
+      $(this).removeClass("dropdown-toggle");
+    }
+  );
+
+  $(".dropdown-button").click(function (e) {
+    e.preventDefault();
   });
 });
