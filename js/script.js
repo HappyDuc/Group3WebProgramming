9361c931dc2ec9f2a9bbc384397135b3f64f9b5e
JakeSincl <23jsincl@glenalmondcollege.co.uk>
2024-09-26 18:35:51 +0100

    Added horizontal bar on order page and removed some unnecessary code.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
 $(function () {
   $(".template").load("/template.html");
 });
-
-var velocity = 0.5;
-
-function update() {
-  var pos = $(window).scrollTop();
-  $(".body").each(function () {
-    var $element = $(this);
-    // subtract some from the height b/c of the padding
-    var height = $element.height() - 18;
-    $(this).css(
-      "backgroundPosition",
-      "50% " + Math.round((height - pos) * velocity) + "px"
-    );
-  });
-}
-
-$(window).bind("scroll", update);
