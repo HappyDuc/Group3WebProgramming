59663e5967aed515935e3b122b03695ba4e3a2b8
JakeSincl <23jsincl@glenalmondcollege.co.uk>
2024-10-03 12:54:43 +0100

    Added "About Us" page.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(function () {
   $(".template").load("/template.html");
 });
+
+const myCarouselElement = document.querySelector("#teamCarousel");
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false,
+});
