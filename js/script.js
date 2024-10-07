967e85e48a5382cdc67eb422f8598bcd93ce7b01
RossBrown1345 <rossmlbrwn@gmail.com>
2024-10-07 14:30:53 +0100

    start made on basket JS

--- js/script.js
+++ js/script.js
@@ -3,8 +3,41 @@ $(function () {
 });
 
 const myCarouselElement = document.querySelector("#teamCarousel");
-
+/*
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false,
 });
+
+*/
+
+
+
+function addItem(basket, foodItem){
+    console.log('item added yay')
+    basket.addItem(foodItem);
+    console.log('item added yay')
+}
+
+
+class FoodItem{
+  constructor(price, name, image) {
+    this.price = price;
+    this.name = name;
+    this.image = image;
+  }
+}
+
+class basket{
+
+constructor(){
+  this.contents = [];
+}
+
+addItem(foodItem){
+    this.contents.push(foodItem)
+    console.log('item added to basket')
+  }
+}
+
+
