e74084d251066102fe7d144201f44fe43d3e8e51
= <=>
2024-11-12 20:38:31 +0000

    Created logic for creating and connecting to a database.

---
+++ databaseConnection.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+
+const databaseConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+});
+databaseConnection.connect(function (err) {
+  if (err) throw err;
+  databaseConnection.query("CREATE DATABASE db", function (err, result) {
+    if (err) throw err;
+    console.log("Database created successfully.");
+  });
+});
+
+module.exports = databaseConnection;
