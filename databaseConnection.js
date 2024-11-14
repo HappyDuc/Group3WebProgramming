0a6b1da96853a04d27ce3735a7ec766ee552e783
CoreyY18 <cy2025@hw.ac.uk>
2024-11-14 13:37:15 +0000

    Just keep this for now

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,18 +1,19 @@
 var mysql = require("mysql");
 
-const databaseConnection = mysql.createConnection({
+const con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
+  database: "mydb"
 });
-/*
-databaseConnection.connect(function (err) {
-  if (err) throw err;
-  databaseConnection.query("CREATE DATABASE db", function (err, result) {
-    if (err) throw err;
-    console.log("Database created successfully.");
-  });
+
+con.connect(function (err) {
+ if(err) throw err;
+ console.log("Connected!");
+ con.query("CREATE DATABASE mydb", function(err, result){
+  if(err) throw err;
+  console.log("Database Created")
+ })
 });
 
 module.exports = databaseConnection;
-*
