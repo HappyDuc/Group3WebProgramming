b2a7fad4317134a9649cf9f47c20f37d997e2a4a
= <=>
2024-11-16 17:09:45 +0000

    Implemented temporary fixes for someone's mess.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,7 @@
 // Object decleration for a food item
 class MainItem {
-  constructor(price, filling, toppings, base, id) { /// this is for the mains
+  constructor(price, filling, toppings, base, id) {
+    /// this is for the mains
     this.price = price;
     this.filling = filling;
     this.toppings = toppings;
@@ -10,7 +11,8 @@ class MainItem {
 }
 
 class DipChip {
-  constructor(price, toppings, base, id) { /// this is for the dip and chip
+  constructor(price, toppings, base, id) {
+    /// this is for the dip and chip
     this.price = price;
     this.toppings = toppings;
     this.base = base;
@@ -19,62 +21,61 @@ class DipChip {
 }
 
 class Churro {
-  constructor(price, base, id) { /// this is for the churros
+  constructor(price, base, id) {
+    /// this is for the churros
     this.price = price;
     this.base = base;
     this.id = id;
   }
 }
 
-class Basket { /// basket will just be an array of items
+class Basket {
+  /// basket will just be an array of items
   constructor() {
     this.contents = [];
   }
 
-  pushItem(item){
+  pushItem(item) {
     this.contents.push(item);
   }
-
 }
-
 
 /// make a function that will take the contents ad add to the basket
 
-
-
-
-function calcTotalPrice(){
+function calcTotalPrice() {
   let basket = JSON.parse(sessionStorage.getItem("basket")); /// get basket from storage
   let baskContents = basket.contents; /// get itemArray
-  
+
   ///console.log("length is "+baskContents.length);
   let totalPrice = 0; ///initial total price
-  for(let item in baskContents){ ///iterate through array
-    
+  for (let item in baskContents) {
+    ///iterate through array
+
     let foodItem = baskContents[item];
     //console.log(item);
     totalPrice += foodItem.price; /// add item price to total
   }
-  $("#total-price").text("Total Price : £ "+totalPrice.toFixed(2)); /// display total price to 2dp
+  $("#total-price").text("Total Price : £ " + totalPrice.toFixed(2)); /// display total price to 2dp
   return totalPrice; /// when using at checkout, and after discount code
 }
 
+var idCounter = 0; /// initialise counter of foodItem ids
 
-var idCounter = 0; /// initialise counter of foodItem ids 
-
-
-function createBasket(){
+function createBasket() {
   const basket = new Basket();
   ///console.log("all done")
-  sessionStorage.setItem("basket", JSON.stringify(basket))
-  
+  sessionStorage.setItem("basket", JSON.stringify(basket));
 }
-
 
 // Function will (hopefully) add a food item to session storage
 function addToCart() {
-  let basket = JSON.parse(sessionStorage.getItem("basket"));
-  console.log(typeof(basket));
+  let basketData = JSON.parse(sessionStorage.getItem("basket"));
+  let basket = new Basket();
+  if (basketData !== null) {
+    for (let index = 0; index < basketData.contents.length; index++) {
+      basket.pushItem(basketData.contents[index]);
+    }
+  }
 
   const base = $("#summary-base").text(); /// this isnt quite right
   const filling = $('input[name="flexRadioDefault"]:checked').val();
@@ -85,61 +86,54 @@ function addToCart() {
 
   idCounter++;
 
-  if (base === "Churros"){
+  if (base === "Churros") {
     const churro = new Churro(4.95, base, idCounter);
     basket.contents.push(churro); /// pushes item to basket []
-  }
-  else if (base === "Dip and Chip"){
-    const dipChip = new DipChip(5.95,toppings, base, idCounter);
+  } else if (base === "Dip and Chip") {
+    const dipChip = new DipChip(5.95, toppings, base, idCounter);
     basket.contents.push(dipChip); /// pushes item to basket []
-  }
-  else{/// item is a main
+  } else {
+    /// item is a main
     const mainItem = new MainItem(price, filling, toppings, base, idCounter);
     basket.contents.push(mainItem); /// pushes item to basket []
   }
   //sessionStorage.setItem(idCounter, JSON.stringify(foodItem));
-  
+
   /// here, instead of pushing to session storage, i need to add it to the basket
 
   sessionStorage.setItem("basket", JSON.stringify(basket));
   defaultForms(); /// resets forms to empty
   defaultSummary();
-  $("#checkout-button").text("Checkout to enjoy : "+idCounter+" Items");
+  $("#checkout-button").text("Checkout to enjoy : " + idCounter + " Items");
   //console.log("addToCart ran");
-
 }
 
-
-function applyDiscount(inputCode){ /// incomplete
-  if (inputCode = "BANANA"){
-
+function applyDiscount(inputCode) {
+  /// incomplete
+  if ((inputCode = "BANANA")) {
   }
 }
 
-
 //resets the forms to unchecked after the item has been added, will occur after itemAdded
-function defaultForms(){
-  $("input[name='flexRadioDefault']").prop('checked', false);
-  $("input[type='checkbox']").prop('checked', false);
+function defaultForms() {
+  $("input[name='flexRadioDefault']").prop("checked", false);
+  $("input[type='checkbox']").prop("checked", false);
 }
 
 //resets the summary to being empty after adding an order to the cart
-function defaultSummary(){ 
+function defaultSummary() {
   $("#summary-base").text("Select Base");
   $("#summary-filling").text("Select Filling");
-  $("#add-to-cart-button").text("item added to cart")
+  $("#add-to-cart-button").text("item added to cart");
   //$(".zoneSeperator").hide(); these lines make it unclear that the order has gone through
   //$("#customize").hide(); rather, a success message should appear
 }
-
 
 $(function () {
   $(".template").load("template");
 });
 
 const myCarouselElement = document.querySelector("#teamCarousel");
-
-
 
 function showCustomize() {
   // Function reveals zone seperator and customize section when a menu item is clicked
@@ -149,12 +143,25 @@ function showCustomize() {
   });
 }
 
-
-
-
-
-
-
+function displayBasket() {
+  console.log("Displaying basket");
+  let basketItemCard = $(".basket-card");
+  console.log("basketItemCard=", basketItemCard);
+  let basketData = JSON.parse(sessionStorage.getItem("basket"));
+  console.log("basketData=", basketData);
+  let basket = new Basket();
+  if (basketData !== null) {
+    for (let index = 0; index < basketData.contents.length; index++) {
+      basket.pushItem(basketData.contents[index]);
+    }
+  }
+  console.log("basket.contents=", basket.contents);
+  for (let index = 0; index < basket.contents.length; index++) {
+    const element = basket.contents[index];
+    let newCard = basketItemCard.clone(true);
+    newCard.insertAfter($("#basketList:last"));
+  }
+}
 
 // Function updates the order summary section when customising an order
 $(document).ready(function () {
@@ -182,7 +189,6 @@ $(document).ready(function () {
   // Event listener for base selection
   $("#menuItemsGrid .card").on("click", function (e) {
     selectedBase = $(this).find(".card-title").text();
-
     // If Dip and Chip or Churros is selected hide appropriate sections (e.g. churros cannot have a topping)
     if (selectedBase === "Dip and Chip") {
       // Hide the fillings section
@@ -213,29 +219,30 @@ $(document).ready(function () {
 
   // Initial update for default selections
   updateSummary();
+  displayBasket();
 });
 
 //Database Input
 $(document).ready(function () {
-  $('#searchBtn').click(function () {
-    var orderID = $('#orderID').val();  // Get the order ID input value
+  $("#searchBtn").click(function () {
+    var orderID = $("#orderID").val(); // Get the order ID input value
 
     if (orderID) {
       $.get(`/lookupOrder?orderID=${orderID}`, function (data) {
         // Display the result
         if (data.message) {
-          $('#orderDetails').html(`<p>${data.message}</p>`);
+          $("#orderDetails").html(`<p>${data.message}</p>`);
         } else {
-          var detailsHtml = '<ul>';
+          var detailsHtml = "<ul>";
           data.forEach(function (item) {
             detailsHtml += `<li>Food Name: ${item.foodName}, Description: ${item.fDescription}, Price: $${item.price}</li>`;
           });
           detailsHtml += `</ul><p>Total Price: $${data[0].Totalprice}</p>`;
-          $('#orderDetails').html(detailsHtml);
+          $("#orderDetails").html(detailsHtml);
         }
       });
     } else {
-      $('#orderDetails').html('<p>Please enter a valid Order ID.</p>');
+      $("#orderDetails").html("<p>Please enter a valid Order ID.</p>");
     }
   });
 });
--- routes/orderLookup.js
+++ routes/orderLookup.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET aboutUs listing. */
+/* GET orderLookup listing. */
 router.get("/", function (req, res, next) {
   res.render("orderLookup", { title: "Express" });
 });
