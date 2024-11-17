9b783abd35967c179438bffc195c1ed7e0fa7dfa
CoreyY18 <cy2025@hw.ac.uk>
2024-11-17 16:08:49 +0000

    ItemCounter and orderLookup feature partially

--- database/data.sql
+++ database/data.sql
@@ -1,25 +1,20 @@
 -- Create the MENU_INFO database
+--This File does nothing its just for storing our code for when we need to submit it.
 CREATE DATABASE MENU_INFO;
 
 -- Select the database
 USE MENU_INFO;
 
--- Create the basket table
-CREATE TABLE basket (
-    orderID INT IDENTITY(1,1), --Should automatically increment each ID by 1
-    Totalprice FLOAT NOT NULL,
-    PRIMARY KEY (orderID)
-);
-
 -- Create the foodContent table (assuming it doesn't exist yet)
 CREATE TABLE foodContent (
-    foodID INT IDENTITY(1,1), --Should automatically increment each ID by 1
+    orderID INT IDENTITY(1,1), --Should automatically increment each ID by 1
     foodName TEXT NOT NULL,
     fDescription TEXT,
     price FLOAT NOT NULL,
     orderID INT,
-    PRIMARY KEY (foodID),
-    FOREIGN KEY (orderID) REFERENCES basket(orderID)
+    itemCount int DEFAULT 1,
+    PRIMARY KEY (orderID),
 );
+
 
 
--- databaseConnection.js
+++ databaseConnection.js
@@ -1,13 +1,21 @@
-const {
-    createPool
-} = require('mysql')
+const { createPool } = require('mysql');
 
-// Create a connection to the database
+// Create a connection pool to the database
 const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'menu_info',
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'menu_info',
+});
+
+// Test the connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL as ID:', connection.threadId);
+  connection.release();
 });
 
 //Testing a query
@@ -18,3 +26,5 @@ pool.query('select * from foodcontent', (err,result,fields)=>{
     }
     return console.log(result);
 })
+
+module.exports = pool;
